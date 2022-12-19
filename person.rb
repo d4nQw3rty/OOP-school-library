7bb4599d41c27f893737fb7417f9ae3e342219ca
d4nQw3rty <danlinmoy@gmail.com>
2022-12-19 15:35:42 -0400

    Create person.rb

---
+++ person.rb
@@ -0,0 +1,8 @@
+class Person
+  def intialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+end
