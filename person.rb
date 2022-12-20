d55b90f4b311396f6c1aa26c2677e6f50aba6768
d4nQw3rty <danlinmoy@gmail.com>
2022-12-20 16:55:59 -0400

    Make Person class inherits from Nameable

--- person.rb
+++ person.rb
@@ -1,8 +1,9 @@
-# Class: Person
-class Person
+require './nameable'
+class Person < Nameable
   # initialize method
 
   def intialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -17,6 +18,10 @@ class Person
 
   def can_use_services?
     is_of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
   # Private methods
 
