46f36b9c84280858aea19c670dc0e1f99e2fd8fe
d4nQw3rty <danlinmoy@gmail.com>
2022-12-21 18:16:58 -0400

    Add the correct arguments in Rental

--- rental.rb
+++ rental.rb
@@ -1,10 +1,11 @@
 require './book'
 class Rental
-  def initialize(date)
+  attr_accessor :date, :book, :person
+  def initialize(date, book, person)
     @date = date
     @person = person
     book.rentals << self
     person.rentals << self
   end
-  attr_accessor :date, :book, :person
+  
 end
