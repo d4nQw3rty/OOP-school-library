1f1b73eaa54adc99a14aea30cc69043fd33ee1ad
d4nQw3rty <danlinmoy@gmail.com>
2022-12-21 18:12:51 -0400

    Add right getter and setters

--- book.rb
+++ book.rb
@@ -1,12 +1,11 @@
 class Book
+  attr_accessor :title, :author, :rentals
   def initialize(title, author)
     @title = title
     @author = author
     @rentals = []
   end
-  attr_accessor :title, :author
-  attr_reader :rentals
-
+ 
   def add_rental(date)
     rentals.push(date, self)
   end
