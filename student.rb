0f5f464ac4db8c150e808467973c519433e69abf
d4nQw3rty <danlinmoy@gmail.com>
2022-12-21 18:15:38 -0400

    set classroom for a student

--- student.rb
+++ student.rb
@@ -4,20 +4,23 @@ require './person'
 require './classroom'
 # Class: Student inherits from Person
 class Student < Person
+  # getter method
+  attr_reader :classroom
+
   # initialize method adds a new instance variable @classroom
 
   def initialize(*person, classroom)
     super(*person)
-    classroom.students << self unless classroom.students.include?(self)
+    @classroom = classroom
+  end
 
-    @classroom =
-      classroom.students << self
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students << self unless classroom.students.include?(self)
   end
 
   # method to play
   def play_hooky
     '¯\(ツ)/¯'
   end
-  # getter method
-  attr_accessor :classroom
 end
