26d7a381915f61ab29a80adff398420c3393ed54
d4nQw3rty <danlinmoy@gmail.com>
2022-12-21 15:37:05 -0400

    Create the belongs-to side in student
    -Create the belongs-to side (a student belongs to a classroom).
    -Make sure that when adding a student to a classroom it also sets the classroom for the student.
    -Make sure that when setting the classroom for a student it also adds it to the classrooms' students.

--- student.rb
+++ student.rb
@@ -1,13 +1,17 @@
 # Importing the Person class from person.rb
 
 require './person'
+require './classroom'
 # Class: Student inherits from Person
-
 class Student < Person
   # initialize method adds a new instance variable @classroom
+
   def initialize(*person, classroom)
     super(*person)
-    @classroom = classroom
+    classroom.students << self unless classroom.students.include?(self)
+
+    @classroom =
+      classroom.students << self
   end
 
   # method to play
