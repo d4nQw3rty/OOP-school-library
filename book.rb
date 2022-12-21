65fe9616a63bdd9d4a76346c4eefc69564aac989
d4nQw3rty <danlinmoy@gmail.com>
2022-12-21 11:50:11 -0400

    Update book.rb

--- book.rb
+++ book.rb
@@ -1,7 +1,7 @@
 class Book
-def initialize(title, author)
-@title = title
-@author = author
-end
-attr_accessor :title, :author
+  def initialize(title, author)
+    @title = title
+    @author = author
+  end
+  attr_accessor :title, :author
 end
