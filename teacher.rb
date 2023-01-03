3620bb28951a120c2c92d53f653103266ddff18b
d4nQw3rty <danlinmoy@gmail.com>
2023-01-03 15:38:43 -0400

    add save_teacher

--- teacher.rb
+++ teacher.rb
@@ -1,4 +1,4 @@
-# require './person'
+require 'json'
 require './person'
 # Class: Teacher inherits from Person
 class Teacher < Person
@@ -12,6 +12,16 @@ class Teacher < Person
   def can_use_services?
     true
   end
+
+  def save_teacher
+    # save teacher to file
+    teacher_json = { id: @id, age: @age, name: @name, parent_permission: @parent_permission,
+                     specialization: @specialization }.to_json
+    File.open('teachers.json', 'a') do |file|
+      file.puts teacher_json
+    end
+  end
+
   # getter method
   attr_accessor :specialization
 end
