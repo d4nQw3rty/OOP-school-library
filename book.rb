f99d45c71d43668bb2acfb19b5b7ed0721889744
d4nQw3rty <danlinmoy@gmail.com>
2023-01-03 15:38:20 -0400

    add save_book into json file

--- book.rb
+++ book.rb
@@ -1,4 +1,5 @@
 require './rental'
+require 'json'
 class Book
   attr_accessor :title, :author
   attr_reader :rentals
@@ -7,6 +8,24 @@ class Book
     @title = title
     @author = author
     @rentals = []
+  end
+
+  def save_book
+    # save book to file
+    book_json = { title: @title, author: @author }.to_json
+    File.open('books.json', 'a') do |file|
+      file.puts book_json
+    end
+  end
+
+  def load_books
+    # load books from file
+    File.open('books.json', 'r') do |file|
+      file.readlines.each do |line|
+        book = JSON.parse(line, symbolize_names: true)
+        @books << Book.new(book[:title], book[:author])
+      end
+    end
   end
 
   def add_rental(date, person)
